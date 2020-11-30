ecb3934b54e3e072d07883fb479c1d401e969e1f
aaknox <azhya.knox@gmail.com>
2020-11-29 23:49:23 -0600

    began setup for finance manager user stories

--- project-1/src/main/webapp/js/admin-home.js
+++ project-1/src/main/webapp/js/admin-home.js
@@ -2,3 +2,15 @@ function goBack(){
 	console.log('return home button clicked');
 	window.location = "http://localhost:8080/project-1/home.html";
 }
+
+var managerId = document.getElementById('user-id');
+var managerName = document.getElementById('user-name');
+
+let user = JSON.parse(sessionStorage.getItem('currentUser'));
+console.log(user);
+
+managerId.innerText = "";
+managerName.innerText = "";
+
+managerId.innerText = user.userId;
+managerName.innerText = `${user.firstName} ${user.lastName}`;
